30da6c3aeeb73094757a9932461a3cdf3690eb1d
August <a.dankevicius@gmail.com>
2019-09-22 18:13:35 +0100

    added files

---
+++ script.js
@@ -0,0 +1,5 @@
+// toggle between hiding and showing dropdown content
+function drop(id,id2) {
+	document.getElementById(id).classList.toggle("show");
+	document.getElementById(id2).classList.toggle("fa-caret-up");
+}
